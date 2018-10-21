97525b5097383254c1e238a3b4f039652d18e378
Jian-Hong Pan <starnight@g.ncu.edu.tw>
2018-10-21 11:13:32 +0800

    Modify image processing order

--- findice.py
+++ findice.py
@@ -7,10 +7,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 org = cv2.imread(sys.argv[1])
-img = cv2.cvtColor(org, cv2.COLOR_BGR2GRAY)
-img = cv2.blur(img,(3,3))
-ret,gray = cv2.threshold(img, 50, 255, cv2.THRESH_TOZERO)
-cv2.imwrite(sys.argv[2], gray)
 
 hist = cv2.calcHist([org],[0],None,[256],[0,256])
 plt.figure()
@@ -21,18 +17,29 @@ plt.plot(hist)
 plt.xlim([0, 256])
 plt.show()
 
-ret,snow_gray = cv2.threshold(img, 50, 255, cv2.THRESH_TOZERO)
-invsnowgray = cv2.bitwise_not(snow_gray)
-snow_bias = 255-invsnowgray.min()
-print('Minimum gray value in this snow_gray {}'.format(snow_bias))
-snow = cv2.addWeighted(snow_gray, 0.1, snow_gray, 0, 0)
-cv2.imwrite(sys.argv[2].replace('.jpg', '-snow.jpg'), snow)
+thres = 50
 
-ret,snow_mask = cv2.threshold(snow_gray, 200, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+img = cv2.cvtColor(org, cv2.COLOR_BGR2GRAY)
+img = cv2.blur(img,(3,3))
+cv2.imwrite(sys.argv[2].replace('.jpg', '-blur.jpg'), img)
+
+ret,gray = cv2.threshold(img, thres, 255, cv2.THRESH_TOZERO)
+cv2.imwrite(sys.argv[2], gray)
+
+ret,snow_gray = cv2.threshold(img, thres, 255, cv2.THRESH_TOZERO)
+invsnowgray = cv2.bitwise_not(snow_gray)
+cv2.imwrite(sys.argv[2].replace('.jpg', '-snow_gray.jpg'), snow_gray)
+
+snow_thres = 200
+ret,snow_mask = cv2.threshold(snow_gray, snow_thres, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 cv2.imwrite(sys.argv[2].replace('.jpg', '-snow_mask.jpg'), snow_mask)
 
-ret,snowland = cv2.threshold(gray, 25, 255, cv2.THRESH_BINARY)
-cv2.imwrite(sys.argv[2].replace('.jpg', '-snowland.jpg'), snowland)
+snow = cv2.bitwise_and(snow_gray, snow_gray, mask=snow_mask)
+snow = cv2.addWeighted(snow, 1, snow, 0, -snow_thres)
+cv2.imwrite(sys.argv[2].replace('.jpg', '-snow.jpg'), snow)
+
+ret,snowland_mask = cv2.threshold(gray, thres, 255, cv2.THRESH_BINARY)
+cv2.imwrite(sys.argv[2].replace('.jpg', '-snowland_mask.jpg'), snowland_mask)
 
 edges = cv2.Canny(img,100,200)
 cv2.imwrite(sys.argv[2].replace('.jpg', '-edge.jpg'), edges)
